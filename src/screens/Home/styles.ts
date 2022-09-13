8034affe81b646a38e56c7e92c406520d86b845b
guferreira1 <gutoconsultor.imoveis@hotmail.com>
2022-09-13 19:58:04 -0300

    feat: landing page mobile

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    marginTop: 70,
+  },
+  logo: {
+    width: 214,
+    height: 120,
+    marginTop: 74,
+    marginBottom: 48
+  },
+
+  contentList: {
+    paddingLeft: 32,
+    paddingRight: 60,
+  }
+});
