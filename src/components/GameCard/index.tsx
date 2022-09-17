2458142a5562f40bef6a1925d2729ff336baeb6c
guferreira1 <gutoconsultor.imoveis@hotmail.com>
2022-09-17 15:00:12 -0300

    first commit

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -6,9 +6,11 @@ import { styles } from './styles';
 
 export interface GameCardProps {
     id: string;
-    name: string;
-    ads: string;
-    cover: ImageSourcePropType;
+    title: string;
+    _count: {
+        ads: number;
+    }
+    bannerUrl: string;
 }
 
 interface Props extends TouchableOpacityProps {
@@ -22,16 +24,16 @@ export function GameCard({ data, ...rest }: Props) {
         
             <ImageBackground 
                 style={styles.cover}
-                source={data.cover}
+                source={{uri : data.bannerUrl}}
             >
 
             <LinearGradient colors={THEME.COLORS.FOOTER} style={styles.footer}>
                 <Text style={styles.name}>
-                    {data.name}
+                    {data.title}
                 </Text>
 
                 <Text style={styles.ads}>
-                    {data.ads} anúncios
+                    {data._count.ads} anúncios
                 </Text>
 
             </LinearGradient>
